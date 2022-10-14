7d7d55ac9dbbfe392b7cf08122dd5481581dbb79
易蓦年 <YiMonian@outlook.com>
2022-10-14 17:04:12 +0800

    v0.4.02

---
+++ Project1/RTL/partfreq_test.v
@@ -0,0 +1,13 @@
+#
+# 验证平台代码
+#
+
+module partfreq_test(
+    input       clk,
+    input       rst,
+    output      clk_out
+);
+
+    .clk        (clk),
+    .rst        (rst),
+    .clk_out    (clk_out)
