684b716cdf85b2e8f27d52d44eb80a40384d12a7
易蓦年 <YiMonian@outlook.com>
2022-10-14 17:14:53 +0800

    v0.4.02(22.10.01)

--- Project1/RTL/partfreq_test.v
+++ Project1/RTL/partfreq_test.v
@@ -5,9 +5,15 @@
 module partfreq_test(
     input       clk,
     input       rst,
-    output      clk_out
+    output reg  clk_out
 );
 
+partfreq    partfreq_test_1(
     .clk        (clk),
     .rst        (rst),
     .clk_out    (clk_out)
+);
+
+
+
+endmodule
