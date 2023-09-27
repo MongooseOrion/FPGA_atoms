2882b2e53b8f82a512b8904a0e21e11193d06a3d
Mongoose <smn9008@outlook.com>
2023-09-27 17:10:06 +0800

    v0.4.11

--- system/axi_controller/axi_master.v
+++ system/axi_controller/axi_master.v
@@ -29,32 +29,35 @@ module axi_master#(
 )(
     input                                   m_axi_aclk,
     input                                   m_axi_aresetn,
+
+    input       [M_AXI_DATA_WIDTH-1'b1:0]   data_in,
+
     // 写地址通道
-    output      [M_AXI_ID_WIDTH-1'b1:0]     m_axi_awid,
-    output      [M_AXI_ADDR_WIDTH-1'b1:0]   m_axi_awaddr,
-    output      [7:0]                       m_axi_awlen,
-    output      [2:0]                       m_axi_awsize,
-    output      [1:0]                       m_axi_awburst,
-    output                                  m_axi_awlock,
-    output      [3:0]                       m_axi_awcache,
-    output      [2:0]                       m_axi_awprot,
-    output      [3:0]                       m_axi_awqos,
-    output      [M_AXI_AWUSER_WIDTH-1'b1:0] m_axi_awuser,
-    output                                  m_axi_awvalid,
-    input                                   m_axi_awready,
+    output      [M_AXI_ID_WIDTH-1'b1:0]     m_axi_awid,     // 写地址 ID
+    output      [M_AXI_ADDR_WIDTH-1'b1:0]   m_axi_awaddr,   // 写地址
+    output      [7:0]                       m_axi_awlen,    // 突发写长度
+    output      [2:0]                       m_axi_awsize,   // 突发写长度参数配置
+    output      [1:0]                       m_axi_awburst,  // 突发类型
+    output                                  m_axi_awlock,   // 锁类型
+    output      [3:0]                       m_axi_awcache,  // 缓存类型
+    output      [2:0]                       m_axi_awprot,   // 保护类型
+    output      [3:0]                       m_axi_awqos,    // 写 QOS 标志符
+    output      [M_AXI_AWUSER_WIDTH-1'b1:0] m_axi_awuser,   
+    output                                  m_axi_awvalid,  // 写地址有效
+    input                                   m_axi_awready,  // 写地址准备
     // 写数据通道
-    output      [M_AXI_DATA_WIDTH-1'b1:0]   m_axi_data,
-    output      [M_AXI_DATA_WIDTH/8-1'b1:0] m_axi_wstrb,
+    output      [M_AXI_DATA_WIDTH-1'b1:0]   m_axi_wdata,
+    output      [M_AXI_DATA_WIDTH/8-1'b1:0] m_axi_wstrb,    // 写选通
     output                                  m_axi_wlast,
     output      [M_AXI_WUSER_WIDTH-1'b1:0]  m_axi_wuser,
     output                                  m_axi_wvalid,
     input                                   m_axi_wready,
     // 写响应通道
-    input       [M_AXI_ID_WIDTH-1'b1:0]     m_axi_bid,
-    input       [1:0]                       m_axi_bresp,
+    input       [M_AXI_ID_WIDTH-1'b1:0]     m_axi_bid,      // 写响应 ID
+    input       [1:0]                       m_axi_bresp,    // 写响应
     input       [M_AXI_BUSER_WIDTH-1'b1:0]  m_axi_buser,
-    input                                   m_axi_bvalid,
-    output                                  m_axi_bready,
+    input                                   m_axi_bvalid,   // 写响应有效
+    output                                  m_axi_bready,   // 写响应准备
     // 读地址通道
     output      [M_AXI_ID_WIDTH-1'b1:0]     m_axi_arid,
     output      [M_AXI_ADDR_WIDTH-1'b1:0]   m_axi_araddr,
@@ -78,7 +81,164 @@ module axi_master#(
     output                                  m_axi_rready
 );
 
-// 计算输入数据的二进制位宽
+// 计算输入数据的二进制位宽，定义突发传输的尺寸
+function integer clogb2(input integer number);
+begin
+    for(clogb2 = 0; number > 0; clogb2 = clogb2 + 1)
+        number = number >> 1;
+end
+endfunction
+
+wire                            clk;
+wire                            rst;
+
+reg [M_AXI_ADDR_WIDTH-1'b1:0]   reg_m_axi_awaddr;
+reg                             reg_m_axi_awvalid;
+reg [M_AXI_DATA_WIDTH-1'b1:0]   reg_m_axi_wdata;
+reg                             reg_m_axi_wlast;
+reg                             reg_m_axi_wvalid;
+reg [M_AXI_ADDR_WIDTH-1'b1:0]   reg_m_axi_araddr;
+reg                             reg_m_axi_arvalid;
+reg                             reg_m_axi_rready;
+
+reg                             write_start;
+reg [7:0]                       burst_data_cnt;
+
+assign m_axi_awid = 'b0;
+assign m_axi_awaddr = reg_m_axi_awaddr + M_SLAVE_BASE_ADDR;
+assign m_axi_awvalid = reg_m_axi_awvalid;
+assign m_axi_awlen = M_AXI_BURST_LEN - 1'b1;
+assign m_axi_awsize = clogb2((M_AXI_DATA_WIDTH/8) - 1'b1);  // 000:1, 001:2, 010:4, 011:8, 100: 16
+assign m_axi_awburst = 2'b01;                               // 00:固定模式, 01:自增模式
+assign m_axi_awlock = 1'b0;
+assign m_axi_awcache = 4'b0010;                             // 0010:无缓存无 buffer
+assign m_axi_awprot = 3'b0;                                 // 000:无特权访问
+assign m_axi_awqos = 4'b0;
+assign m_axi_awuser = 'b0;
+
+assign m_axi_wdata = reg_m_axi_wdata;
+assign m_axi_wstrb = {(M_AXI_DATA_WIDTH/8){1'b1}};
+assign m_axi_wlast = reg_m_axi_wlast;
+assign m_axi_wuser = 'b0;
+assign m_axi_wvalid = reg_m_axi_wvalid;
+
+assign m_axi_bready = 1'b1;
+
+assign m_axi_arid = 'b0;
+assign m_axi_araddr = reg_m_axi_araddr + M_SLAVE_BASE_ADDR;
+assign m_axi_arlen = M_AXI_BURST_LEN - 1'b1;
+assign m_axi_arsize = clogb2((M_AXI_DATA_WIDTH/8) - 1'b1);
+assign m_axi_arburst = 2'b01;
+assign m_axi_arlock = 1'b0;
+assign m_axi_arcache = 4'b0010;
+assign m_axi_arprot = 3'b0;
+assign m_axi_arqos = 4'b0;
+assign m_axi_aruser = 'b0;
+assign m_axi_arvalid = reg_m_axi_arvalid;
+
+assign m_axi_rready = reg_m_axi_rready;
+
+assign clk = m_axi_aclk;
+assign rst = m_axi_aresetn;
+
+
+// 写地址有效信号
+always @(posedge clk or negedge rst) begin
+    if(!rst) begin
+        reg_m_axi_awvalid <= 'b0;
+    end
+    else if(write_start) begin              // 在此标志信号后开始写操作
+        reg_m_axi_awvalid <= 1'b1;
+    end
+    else if((m_axi_awvalid == 1'b1) && (m_axi_awready == 1'b1)) begin
+        reg_m_axi_awvalid <= 1'b0;
+    end
+    else begin
+        reg_m_axi_awvalid <= reg_m_axi_awvalid;
+    end
+end
+
+
+// 写首地址，此处控制为 1
+always @(posedge clk or negedge rst) begin
+    if(!rst) begin
+        reg_m_axi_awaddr <= 'b0;
+    end
+    else if(write_start) begin
+        reg_m_axi_awaddr <= 'd1;
+    end
+    else begin
+        reg_m_axi_awaddr <= 'd0;
+    end
+end
+
+
+// 写有效信号，只要能保证 awvalid 和 awready 同时为高时该信号已经为高即可
+always @(posedge clk or negedge rst) begin
+    if(!rst) begin
+        reg_m_axi_wvalid <= 'b0;
+    end
+    else if(write_start) begin      // 选择与 awvalid 同时拉高
+        reg_m_axi_wvalid <= 1'b1;
+    end
+    else if(m_axi_wlast) begin
+        reg_m_axi_wvalid <= 1'b0;
+    end
+    else begin
+        reg_m_axi_wvalid <= reg_m_axi_wvalid;
+    end
+end
+
+
+// 写数据信号
+always @(posedge clk or negedge rst) begin
+    if(!rst) begin
+        reg_m_axi_wdata <= 'b0;
+    end
+    else if((m_axi_awvalid == 1'b1) && (m_axi_awready == 1'b1)) begin
+        reg_m_axi_wdata <= data_in;
+    end
+    else if(m_axi_wlast) begin
+        reg_m_axi_wdata <= 'b0;
+    end
+    else begin
+        reg_m_axi_wdata <= reg_m_axi_wdata;
+    end
+end
+
+
+// 写数据计数
+// 只适用于突发长度大于 2 的情况
+always @(posedge clk or negedge rst) begin
+    if(!rst) begin
+        burst_data_cnt <= 8'b0;
+    end
+    else if((m_axi_wready == 1'b1) && (m_axi_wvalid == 1'b1)) begin
+        burst_data_cnt <= burst_data_cnt + 1'b1;
+    end
+    else if(burst_data_cnt == M_AXI_BURST_LEN - 1'b1) begin
+        burst_data_cnt <= 8'b0;
+    end
+end
+
+// 写最后一个数据标志
+always @(posedge clk or negedge rst) begin
+    if(!rst) begin
+        reg_m_axi_wlast <= 'b0;
+    end
+    else if(burst_data_cnt == M_AXI_BURST_LEN - 2'd2) begin
+        reg_m_axi_wlast <= 1'b1;
+    end
+    else begin
+        reg_m_axi_wlast <= 1'b0;
+    end
+end
+
+
+// 写响应准备
+
+
+// 读地址有效信号
 
 
 endmodule
